d2a32a01dbc4419cc377a1736da9fd4d9447b966
FilipJF <filipfelberg@gmail.com>
2016-04-13 14:22:54 +0200

    Fiks Sort

--- Sort.java
+++ Sort.java
@@ -1,5 +1,43 @@
+import java.util.Scanner;
+import java.io.File;
+
 public class Sort{
   public static void main(String[] args) {
-    System.out.println("Test 2");
+    int antTraader = Integer.parseInt(args[0]);
+    File innFil = new File(args[1]);
+    String navnUtFil = args[2];
+
+    Scanner in = new Scanner(innFil);
+    String[] navnTabell = lesInnFil(in);
+
+    int antPerTraad = navnTabell.length / antTraader;
+    String[][] tabellArray = new String[antTraader][antPerTraad];
+
+    int teller = 0;
+    int tellerTabell = 0;
+
+    for(String s : navnTabell){
+      if(teller == antPerTraad){
+        teller = 0;
+        tellerTabell++;
+      }
+      tabellArray[tellerTabell][teller] = s;
+      teller++;
+    }
+  }
+
+  public static String[] lesInnFil(Scanner in){
+    String[] navnTabell = Integer.parseInt(in.nextLine());
+    int teller = 0;
+
+    while(in.hasNextLine()){
+      navnTabell[teller] = in.nextLine();
+      teller++;
+    }
+    return navnTabell;
+  }
+
+  public static void skrivUtFil(){
+
   }
 }
