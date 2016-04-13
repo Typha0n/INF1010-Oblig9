776a87b2f35cf7fe010e8f09189cacfbbc41b471
FilipJF <filipfelberg@gmail.com>
2016-04-13 13:58:11 +0200

    Første økt

---
+++ Sorterer.java
@@ -0,0 +1,18 @@
+public class Sorterer extends Thread{
+
+  private String[] tabell;
+
+  public Sorterer(String[] tabell){
+    this.tabell = tabell;
+  }
+
+  public void run(){
+    for(String s : tabell){
+
+    }
+  }
+
+  public String[] flette(String[] tabellEn, String[] tabellTo){
+
+  }
+}
